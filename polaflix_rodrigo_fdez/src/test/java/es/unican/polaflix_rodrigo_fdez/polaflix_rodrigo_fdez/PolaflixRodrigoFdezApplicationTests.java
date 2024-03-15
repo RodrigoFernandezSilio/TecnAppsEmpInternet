07661a7e7bd00ff579b57956c0e17e89b3d1a1a8
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-03-15 17:41:38 +0100

    Commit inicial

---
+++ polaflix_rodrigo_fdez/src/test/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/PolaflixRodrigoFdezApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolaflixRodrigoFdezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
