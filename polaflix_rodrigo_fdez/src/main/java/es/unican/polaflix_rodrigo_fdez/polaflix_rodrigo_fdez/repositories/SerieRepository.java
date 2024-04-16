c5f3c17bbc6d22f147a36736e8d42104f8fe2443
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-04-16 15:43:35 +0200

    Crear repositorios

---
+++ polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/repositories/SerieRepository.java
@@ -0,0 +1,9 @@
+package es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Serie;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+}
---
+++ polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String>{
+
+}
