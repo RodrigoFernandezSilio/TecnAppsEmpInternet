e7338260b4986389789a44a013390ed90240ba67
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-05-10 01:21:24 +0200

    Eliminar atributo serie

--- polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/domain/CapituloVisto.java
+++ polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/domain/CapituloVisto.java
@@ -1,11 +1,9 @@
 package es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,14 +18,9 @@ import lombok.ToString;
 @ToString
 @Entity
 public class CapituloVisto {
-
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-    
-    @NonNull
-    @ManyToOne
-    private Serie serie;
 
     @NonNull
     private Integer numTemporada;
