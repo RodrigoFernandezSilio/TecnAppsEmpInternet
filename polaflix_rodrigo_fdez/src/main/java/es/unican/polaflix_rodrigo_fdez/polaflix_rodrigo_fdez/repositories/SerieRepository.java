7e27a6bc77df9827ef333c6f36cdb93ecfbc46d2
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-06-01 13:54:07 +0200

    Agregar consulta personalizada para buscar series que comiencen con un numero

--- polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/repositories/SerieRepository.java
+++ polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/repositories/SerieRepository.java
@@ -3,6 +3,7 @@ package es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Serie;
 
@@ -12,4 +13,7 @@ public interface SerieRepository extends JpaRepository<Serie, Long>{
     List<Serie> findByNombre(String nombre);
 
     List<Serie> findByNombreStartingWith(char letraInicial);
+
+    @Query(value = "SELECT * FROM Serie WHERE nombre REGEXP '^[0-9]'", nativeQuery = true)
+    List<Serie> findByNombreStartingWithNumber();
 }
