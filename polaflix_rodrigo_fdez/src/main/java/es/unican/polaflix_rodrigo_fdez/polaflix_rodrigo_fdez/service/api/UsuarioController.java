82ddf3fa29dbaef1d66fe81ca95f0a5923e2d516
Rodrigo Fernandez Silio <rodrigo.fernandezs@alumnos.unican.es>
2024-06-19 21:35:01 +0200

    Corrección: Cambiar PATCH a PUT y pasar parámetros por la URL

--- polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/service/api/UsuarioController.java
+++ polaflix_rodrigo_fdez/src/main/java/es/unican/polaflix_rodrigo_fdez/polaflix_rodrigo_fdez/service/api/UsuarioController.java
@@ -7,9 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +22,6 @@ import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Factura;
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Serie;
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Temporada;
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.domain.Usuario;
-import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.dto.TemporadaCapituloDTO;
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.dto.UsuarioSerieDTO;
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.repositories.SerieRepository;
 import es.unican.polaflix_rodrigo_fdez.polaflix_rodrigo_fdez.repositories.UsuarioRepository;
@@ -36,7 +34,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @RestController
 @RequestMapping("/usuarios")
 @Tag(name = "UsuarioController", description = "Controlador para gestionar operaciones relacionadas con usuarios, como la obtención de detalles de usuario y la gestión de sus datos relacionados con series y visualización de contenido.")
-@CrossOrigin(origins = "http://localhost:4200", maxAge=3600) // Permitir solicitudes desde http://localhost:4200
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600) // Permitir solicitudes desde http://localhost:4200
 public class UsuarioController {
 
     @Autowired
@@ -65,8 +63,8 @@ public class UsuarioController {
     @JsonView(Views.UsuarioSerieDTO_Vista.class)
     @Operation(summary = "Obtener información de una serie por usuario", description = "Devuelve la información de una serie específica para un usuario específico")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Información de la serie encontrada"),
-        @ApiResponse(responseCode = "404", description = "Usuario o serie no encontrados", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Información de la serie encontrada"),
+            @ApiResponse(responseCode = "404", description = "Usuario o serie no encontrados", content = @Content)
     })
     public ResponseEntity<UsuarioSerieDTO> obtenerInformacionSeriePorUsuario(
             @PathVariable("usuarioID") String usuarioID,
@@ -93,17 +91,18 @@ public class UsuarioController {
         }
     }
 
-    @PatchMapping("/{usuarioID}/series/{serieID}")
+    @PutMapping("/{usuarioID}/series/{serieID}/temporadas/{numTemporada}/capitulos/{numCapitulo}")
     @Transactional
     @Operation(summary = "Anotar capítulo como reproducido", description = "Marca un capítulo como reproducido para un usuario específico")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Capítulo anotado como reproducido", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Usuario o serie no encontrados", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Capítulo anotado como reproducido", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Usuario o serie no encontrados", content = @Content)
     })
     public ResponseEntity<String> anotarCapituloComoReproducido(
             @PathVariable("usuarioID") String usuarioID,
             @PathVariable("serieID") Long serieID,
-            @RequestBody TemporadaCapituloDTO temporadaCapituloDTO) {
+            @PathVariable("numTemporada") int numTemporada,
+            @PathVariable("numCapitulo") int numCapitulo) {
 
         Optional<Usuario> usuarioOptional = ur.findById(usuarioID);
         Optional<Serie> serieOptional = sr.findById(serieID);
@@ -113,13 +112,12 @@ public class UsuarioController {
                 Usuario usuario = usuarioOptional.get();
                 Serie serie = serieOptional.get();
 
-                Temporada temporada = serie.getTemporadas().get(temporadaCapituloDTO.getNumTemporada() - 1);
-                Capitulo capitulo = temporada.getCapitulos().get(temporadaCapituloDTO.getNumCapitulo() - 1);
+                Temporada temporada = serie.getTemporadas().get(numTemporada - 1);
+                Capitulo capitulo = temporada.getCapitulos().get(numCapitulo - 1);
 
-                System.out.println(capitulo.getTitulo() + capitulo.getNumCapitulo());
                 usuario.anotarCapituloComoReproducido(capitulo);
 
-                return ResponseEntity.ok("Capítulo anotado como reproducido.");
+                return ResponseEntity.ok("Capítulo anotado como reproducido");
             } else {
                 return ResponseEntity.notFound().build();
             }
@@ -128,16 +126,16 @@ public class UsuarioController {
         }
     }
 
-    @PatchMapping("/{usuarioID}/series")
+    @PutMapping("/{usuarioID}/series/{serieID}")
     @Transactional
     @Operation(summary = "Agregar serie al espacio personal", description = "Agrega una serie al espacio personal del usuario")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "204", description = "Serie agregada al espacio personal del usuario", content = @Content),
-        @ApiResponse(responseCode = "404", description = "Usuario o serie no encontrados", content = @Content)
+            @ApiResponse(responseCode = "204", description = "Serie agregada al espacio personal del usuario", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Usuario o serie no encontrados", content = @Content)
     })
     public ResponseEntity<Void> agregarSerieAEspacioPersonal(
             @PathVariable("usuarioID") String usuarioID,
-            @RequestBody Long serieID) {
+            @PathVariable("serieID") Long serieID) {
 
         Optional<Usuario> usuarioOptional = ur.findById(usuarioID);
         Optional<Serie> serieOptional = sr.findById(serieID);
@@ -162,8 +160,8 @@ public class UsuarioController {
     @JsonView(Views.Factura_Vista.class)
     @Operation(summary = "Obtener factura por año y mes", description = "Devuelve la factura de un usuario específico para un año y mes determinados")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Factura encontrada"),
-        @ApiResponse(responseCode = "404", description = "Usuario no encontrado", content = @Content)
+            @ApiResponse(responseCode = "200", description = "Factura encontrada"),
+            @ApiResponse(responseCode = "404", description = "Usuario no encontrado", content = @Content)
     })
     public ResponseEntity<Factura> obtenerFacturasPorAnhoYMes(
             @PathVariable("usuarioID") String usuarioID,
